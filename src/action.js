ec201aef27473e0c9757d29eaa8fc6f106ae92e7
splittesting <a.jamieali@gmail.com>
2022-09-22 18:03:31 -0400

    neww file additions

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-    console.log('Comment');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { issue } = context.payload;
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: issue.issue_number,
+        body: 'Issue closed. Please leave feedback'
+    });
 }
 
 run();
